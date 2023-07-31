cb320d4124ef5ca046ce0438f83a7e2f8a757864
Razvan Paisa <razvysme@yahoo.com>
2023-07-31 10:40:59 +0200

    Restoring to previously working
    Building was working here

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,4 +11,18 @@ const config = {
 	}
 };
 
-export default config;
+//export default config;
+
+export default {
+    kit: {
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        })
+    }
+};
