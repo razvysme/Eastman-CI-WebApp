bfeceac61a065c6108a74f8fdac37c27cdcc54a5
Razvan Paisa <razvysme@yahoo.com>
2023-09-22 17:37:11 +0200

    Changed all audio to personal hosting

--- src/audioData.js
+++ src/audioData.js
@@ -3,28 +3,28 @@ export const audioData = [
 		name: "Imagine",
 		lesson: "Lektion 1",
 		artist: "John Lenon - 1",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/1-1.mp3",
+		url: "https://paisa.dk/TrainingAudio/1-1.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/1.png"
 	},
 	{
 		name: "Imagine",
 		lesson: "Lektion 2",
 		artist: "John Lenon - 1",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/1-2.mp3",
+		url: "https://paisa.dk/TrainingAudio/1-2.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/1.png"
 	},
 	{
 		name: "Imagine",
 		lesson: "Lektion 3",
 		artist: "John Lenon - 1",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/1-3.mp3",
+		url: "https://paisa.dk/TrainingAudio/1-3.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/1.png"
 	},
 	{
 		name: "Imagine",
 		lesson: "Lektion 4",
 		artist: "John Lenon -1",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/1-4.mp3",
+		url: "https://paisa.dk/TrainingAudio/1-4.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/1.png"
 	},
 	
@@ -32,56 +32,56 @@ export const audioData = [
 		name: "Forårsdag",
 		lesson: "Lektion 1",
 		artist: "Anne Linnet - 2",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/2-1.mp3",
+		url: "https://paisa.dk/TrainingAudio/2-1.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/2.png"
 	},
 	{
 		name: "Forårsdag",
 		lesson: "Lektion 2",
 		artist: "Anne Linnet - 2",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/2-2.mp3",
+		url: "https://paisa.dk/TrainingAudio/2-2.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/2.png"
 	},
 	{
 		name: "Forårsdag",
 		lesson: "Lektion 3",
 		artist: "Anne Linnet - 2",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/2-3.mp3",
+		url: "https://paisa.dk/TrainingAudio/2-3.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/2.png"
 	},
 	{
 		name: "Forårsdag",
 		lesson: "Lektion 4",
 		artist: "Anne Linnet - 2",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/2-4.mp3",
+		url: "https://paisa.dk/TrainingAudio/2-4.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/2.png"
 	},
 	{
 		name: "Papirsklip",
 		lesson: "Lektion 1",
 		artist: "Kim Larsen - 3",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/3-1.mp3",
+		url: "https://paisa.dk/TrainingAudio/3-1.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/3.png"
 	},
 	{
 		name: "Papirsklip",
 		lesson: "Lektion 2",
 		artist: "Kim Larsen - 3",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/3-2.mp3",
+		url: "https://paisa.dk/TrainingAudio/3-2.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/3.png"
 	},
 	{
 		name: "Papirsklip",
 		lesson: "Lektion 3",
 		artist: "Kim Larsen - 3",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/3-3.mp3",
+		url: "https://paisa.dk/TrainingAudio/3-3.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/3.png"
 	},
 	{
 		name: "Papirsklip",
 		lesson: "Lektion 4",
 		artist: "Kim Larsen - 3",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/3-4.mp3",
+		url: "https://paisa.dk/TrainingAudio/3-4.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/3.png"
 	},
 	{
@@ -118,28 +118,28 @@ export const audioData_Normal = [
 		name: "Imagine",
 		lesson: "Lektion 1",
 		artist: "John Lenon - 1",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/Normal/1-1.mp3",
+		url: "https://paisa.dk/TrainingAudio/Normal/1-1.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/1.png"
 	},
 	{
 		name: "Imagine",
 		lesson: "Lektion 2",
 		artist: "John Lenon - 1",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/Normal/1-2.mp3",
+		url: "https://paisa.dk/TrainingAudio/Normal/1-2.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/1.png"
 	},
 	{
 		name: "Imagine",
 		lesson: "Lektion 3",
 		artist: "John Lenon - 1",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/Normal/1-3.mp3",
+		url: "https://paisa.dk/TrainingAudio/Normal/1-3.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/1.png"
 	},
 	{
 		name: "Imagine",
 		lesson: "Lektion 4",
 		artist: "John Lenon -1",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/Normal/1-4.mp3",
+		url: "https://paisa.dk/TrainingAudio/Normal/1-4.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/1.png"
 	},
 	
@@ -147,56 +147,56 @@ export const audioData_Normal = [
 		name: "Forårsdag",
 		lesson: "Lektion 1",
 		artist: "Anne Linnet - 2",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/Normal/2-1.mp3",
+		url: "https://paisa.dk/TrainingAudio/Normal/2-1.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/2.png"
 	},
 	{
 		name: "Forårsdag",
 		lesson: "Lektion 2",
 		artist: "Anne Linnet - 2",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/Normal/2-2.mp3",
+		url: "https://paisa.dk/TrainingAudio/Normal/2-2.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/2.png"
 	},
 	{
 		name: "Forårsdag",
 		lesson: "Lektion 3",
 		artist: "Anne Linnet - 2",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/Normal/2-3.mp3",
+		url: "https://paisa.dk/TrainingAudio/Normal/2-3.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/2.png"
 	},
 	{
 		name: "Forårsdag",
 		lesson: "Lektion 4",
 		artist: "Anne Linnet - 2",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/Normal/2-4.mp3",
+		url: "https://paisa.dk/TrainingAudio/Normal/2-4.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/2.png"
 	},
 	{
 		name: "Papirsklip",
 		lesson: "Lektion 1",
 		artist: "Kim Larsen - 3",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/Normal/3-1.mp3",
+		url: "https://paisa.dk/TrainingAudio/Normal/3-1.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/3.png"
 	},
 	{
 		name: "Papirsklip",
 		lesson: "Lektion 2",
 		artist: "Kim Larsen - 3",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/Normal/3-2.mp3",
+		url: "https://paisa.dk/TrainingAudio/Normal/3-2.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/3.png"
 	},
 	{
 		name: "Papirsklip",
 		lesson: "Lektion 3",
 		artist: "Kim Larsen - 3",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/Normal/3-3.mp3",
+		url: "https://paisa.dk/TrainingAudio/Normal/3-3.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/3.png"
 	},
 	{
 		name: "Papirsklip",
 		lesson: "Lektion 4",
 		artist: "Kim Larsen - 3",
-		url: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/audio/Normal/3-4.mp3",
+		url: "https://paisa.dk/TrainingAudio/Normal/3-4.mp3",
 		cover: "https://raw.githubusercontent.com/razvysme/CI-Training-WebApp/main/src/lib/img/3.png"
 	},
 	{
